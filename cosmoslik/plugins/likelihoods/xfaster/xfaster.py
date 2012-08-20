f77cc80ab4671d95b308d5af60770aabacc6e052
marius <mmillea@ucdavis.edu>
2012-08-20 08:57:34 -0700

    made subprocess

--- cosmoslik/plugins/likelihoods/xfaster/xfaster.py
+++ cosmoslik/plugins/likelihoods/xfaster/xfaster.py
@@ -1,6 +1,6 @@
 from cosmoslik.plugins import Likelihood
-from xfaster_likelihood_mod import xfaster_likelihood_mod as X
 from numpy import arange, pi
+from cosmoslik.plugins import SubprocessExtension
 
 class xfaster(Likelihood):
     
@@ -8,16 +8,19 @@ class xfaster(Likelihood):
         return ['cl_TT','cl_TE','cl_EE','cl_BB','egfs']
     
     def init(self, p):
-        X.xfaster_init(p['xfaster','file'])
+        self.X = SubprocessExtension('xfaster_likelihood_mod',globals())
+        self.X.xfaster_init(p['xfaster','file'])
         self.fluxcut = p['xfaster','fluxcut']
         self.freqs = p['xfaster','freqs']
         
     def lnl(self, p, model):
         
-        def to_cl(dl): return dl/(lambda l: l*(l+1)/2/pi)(arange(len(dl)))
+        def to_cl(dl): 
+            dl[2:]/=(lambda l: l*(l+1)/2/pi)(arange(2,len(dl)))
+            return dl
         
         xf_model = {}
         for x in ['cl_TT','cl_TE','cl_EE','cl_BB']:
             xf_model[x.lower()] = to_cl(model[x] + model['egfs'](x, fluxcut=self.fluxcut, freqs=self.freqs, lmax=len(model[x])))[2:]
         
-        return X.xfaster_lnl(**xf_model)
+        return self.X.xfaster_lnl(**xf_model)
