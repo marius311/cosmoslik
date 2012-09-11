ab1ce74e4f8d87d377ecaff15d39f409232de9ff
marius <mmillea@ucdavis.edu>
2012-09-11 01:29:17 -0700

    fixed baseline_cleaning

--- cosmoslik/plugins/models/planck_egfs/baseline_cleaning.py
+++ cosmoslik/plugins/models/planck_egfs/baseline_cleaning.py
@@ -35,31 +35,35 @@ class baseline_cleaning(egfs):
         
         fr1, fr2 = freqs
         
-        lowfrcomps = {'dgpo': lowp['dgpo','amp'] * (arange(lmax)/3000.)**2 * plaw_dep(fr1['dust'], fr2['dust'], lowp['dgpo','norm_fr'], lowp['dgpo','alpha']),
-                      'dgcl_lin': lowp['dgcl','amp_lin'] * self.clustered_template[:lmax] * plaw_dep(fr1['dust'], fr2['dust'], lowp['dgcl','norm_fr'], lowp['dgcl','alpha']),
-                      'dgcl_nonlin': lowp['dgcl','amp_nonlin'] * (arange(lmax)/self.norm_ell)**p.get(('dgcl','tilt' ),0.8) * plaw_dep(fr1['dust'], fr2['dust'], lowp['dgcl','norm_fr'], lowp['dgcl','alpha']),
-                      'radio': lowp['radio','amp'] * (fluxcut / lowp['radio','norm_fluxcut']) ** (2+lowp['radio','gamma']) * (arange(lmax)/3000.)**2 * plaw_dep(fr1['radio'], fr2['radio'], lowp['radio','norm_fr'], lowp['radio','alpha']),
-                      'tsz': lowp['tsz','amp'] * self.tsz_template[:lmax] * tszdep(fr1['tsz'],fr2['tsz'],lowp['tsz','norm_fr']),
-                      'ksz': lowp['ksz','amp'] * self.ksz_template[:lmax]}
+        dustcomp = {}
         
-        highfrcomps = {'dgpo': highp['dgpo','amp'] * (arange(lmax)/3000.)**2, 
-                       'dgcl_lin': highp['dgcl','amp_lin'] * self.clustered_template[:lmax],
-                       'dgcl_nonlin': highp['dgcl','amp_nonlin'] * (arange(lmax)/self.norm_ell)**p.get(('dgcl','tilt' ),0.8),
-                       'tsz': lowp['tsz','amp'] * self.tsz_template[:lmax] * tszdep(fr1['tsz'],fr2['tsz'],lowp['tsz','norm_fr']),
-                       'ksz': lowp['ksz','amp'] * self.ksz_template[:lmax]}
-        
+        for i,fr in enumerate(freqs):
+            if fr['dust']<300:
+                dustcomp[i] = {'dgpo': sqrt(lowp['dgpo','amp']) * (arange(lmax)/3000.) * plaw_dep(fr['dust'], lowp['dgpo','norm_fr'], lowp['dgpo','alpha']),
+                               'dgcl_lin': sqrt(lowp['dgcl','amp_lin'] * self.clustered_template[:lmax]) * plaw_dep(fr['dust'], lowp['dgcl','norm_fr'], lowp['dgcl','alpha']),
+                               'dgcl_nonlin': sqrt(lowp['dgcl','amp_nonlin'] * (arange(lmax)/self.norm_ell)**p.get(('dgcl','tilt' ),0.8)) * plaw_dep(fr['dust'], lowp['dgcl','norm_fr'], lowp['dgcl','alpha'])}
+            else:
+                dustcomp[i] = {'dgpo': sqrt(highp['dgpo','amp']) * (arange(lmax)/3000.), 
+                               'dgcl_lin': sqrt(highp['dgcl','amp_lin'] * self.clustered_template[:lmax]),
+                               'dgcl_nonlin': sqrt(highp['dgcl','amp_nonlin'] * (arange(lmax)/self.norm_ell)**p.get(('dgcl','tilt' ),0.8))}
+
+
         ffr1, ffr2 = fr1['dust'], fr2['dust']
-        if ffr1 > 300 and ffr2 > 300: 
-            return highfrcomps
-        if ffr1 > 300 or ffr2 > 300:
-            lowfr = min(ffr1,ffr2)
-            corr = highp['dgpo','cor143'] if 130<lowfr<150 else highp['dgpo','cor217']
-            for x in highfrcomps:
-                if x.startswith('dg'): highfrcomps[x] = corr*sqrt(highfrcomps[x]*lowfrcomps[x])
-            return highfrcomps
-        else: 
-            return lowfrcomps
+
+        if 130<ffr1<150 and ffr2>300 or ffr1>300 and 130<ffr2<150: corr = highp['dgpo','cor143']
+        if 210<ffr1<230 and ffr2>300 or ffr1>300 and 210<ffr2<230: corr = highp['dgpo','cor217']
+        else: corr = 1
         
+        comps = {}
+        for x in ['dgpo','dgcl_lin','dgcl_nonlin']:
+            comps[x] = corr*dustcomp[0][x]*dustcomp[1][x]
+            
+        comps.update({'radio': lowp['radio','amp'] * (fluxcut / lowp['radio','norm_fluxcut']) ** (2+lowp['radio','gamma']) * (arange(lmax)/3000.) * plaw_dep2(fr1['radio'], fr2['radio'], lowp['radio','norm_fr'], lowp['radio','alpha']),
+                      'tsz': lowp['tsz','amp'] * self.tsz_template[:lmax] * tszdep(fr1['tsz'],fr2['tsz'],lowp['tsz','norm_fr']),
+                      'ksz': lowp['ksz','amp'] * self.ksz_template[:lmax]})
+            
+        return comps
+    
     
 def dBdT(fr1,fr0):
     """ dB/dT at T_CMB """
@@ -71,9 +75,14 @@ def tszdep(fr1,fr2,fr0):
     t1,t2,t0 = map(lambda fr: (lambda x0: x0*(exp(x0)+1)/(exp(x0)-1) - 4)(fr/56.78),[fr1,fr2,fr0])
     return t1*t2/t0**2
 
-def plaw_dep(fr1,fr2,fr0,alpha):
+def plaw_dep(fr,fr0,alpha):
     """A power-law frequency dependence."""
-    return (fr1*fr2/fr0**2.)**alpha / dBdT(fr1,fr0) / dBdT(fr2,fr0)
+    return (fr/fr0)**alpha / dBdT(fr,fr0)
+
+def plaw_dep2(fr1,fr2,fr0,alpha):
+    """A power-law frequency dependence."""
+    return (fr1*fr2/fr0**2)**alpha / dBdT(fr1,fr0) / dBdT(fr1,fr0)
+
 
 def _unpicklable(): pass 
 
