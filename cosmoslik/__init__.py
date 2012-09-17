6098a377c5ad28cebb31539e8018a0bff459e978
marius <mmillea@ucdavis.edu>
2012-09-17 00:47:42 -0700

    converted to namespace packages

--- cosmoslik/__init__.py
+++ cosmoslik/__init__.py
@@ -1,2 +1,6 @@
+#The magic line to make this a "namespace" package.
+#see http://packages.python.org/distribute/setuptools.html#namespace-packages
+__path__ = __import__('pkgutil').extend_path(__path__,__name__)
+
 from cosmoslik import *
 import params, chains
