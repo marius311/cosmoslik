e91b98a8fceac85c6a4933b37b8035506bbde4d0
marius <mmillea@ucdavis.edu>
2013-04-22 20:29:21 -0700

    wmap f2py case

--- cosmoslik_plugins/likelihoods/wmap/pywmap.f90
+++ cosmoslik_plugins/likelihoods/wmap/pywmap.f90
@@ -11,7 +11,7 @@ logical :: pywmap_initalized
 
 contains
 
-    subroutine WMAPInit(tt_min,tt_max,te_min,te_max,data_dir)
+    subroutine wmapinit(tt_min,tt_max,te_min,te_max,data_dir)
         integer :: tt_min, tt_max, te_min, te_max
 
 		character(len=*) :: data_dir
@@ -28,13 +28,13 @@ contains
 
 
     ! To call from Python use WMAPLnLike(cl_tt) where cl_tt[0] is ell=2
-    function WMAPLnLike(cltt, clte, clee, clbb, lmax)
-      real(8), dimension(10) :: WMAPLnLike
+    function wmaplnlike(cltt, clte, clee, clbb, lmax)
+      real(8), dimension(10) :: wmaplnlike
       real(8), dimension(num_WMAP) :: like
       real(8), dimension(2:lmax) :: cltt,clte,clee,clbb
       integer lmax
 
-      WMAPLnLike = 1e30
+      wmaplnlike = 1e30
       if (lmax < ttmax) then
          print *, "WMAPLnLike: Please provide c_ell's up to", ttmax
          return
@@ -48,8 +48,8 @@ contains
       call wmap_likelihood_compute(cltt(2:ttmax),clte(2:ttmax),clee(2:ttmax),clbb(2:ttmax),like)
       
       if (wmap_likelihood_ok) then
-         WMAPLnLike = 0
-         WMAPLnLike(1:num_WMAP) = like
+         wmaplnlike = 0
+         wmaplnlike(1:num_WMAP) = like
       else
          print *, "WMAPLnLike: Error computing WMAP likelihood."
          call wmap_likelihood_error_report
