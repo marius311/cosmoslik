0318026d5e41c0368d33a9d9a569a16617730d36
marius <mmillea@ucdavis.edu>
2013-07-23 14:03:14 -0700

    cosmology model

---
+++ cosmoslik_plugins/models/cosmology.py
@@ -0,0 +1,23 @@
+from cosmoslik import SlikPlugin
+
+class cosmology(SlikPlugin):    
+    
+    def __init__(self,
+                 As = 2.4e-9,
+                 ns = 0.96,
+                 ombh2 = 0.0225,
+                 omch2 = 0.12,
+                 tau = 0.09,
+                 H0 = 70,
+                 massive_neutrinos = 3.046,
+                 massless_neutrinos = 0,
+                 omk = 0,
+                 omnuh2 = 0,
+                 nrun = 0,
+                 w = -1,
+                 **kwargs):
+        
+        super(cosmology,self).__init__(**kwargs)
+        self.update(locals())
+        self.pop('self')
+        self.pop('kwargs')
