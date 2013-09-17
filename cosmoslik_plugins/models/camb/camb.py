9eea6e241b8147403bb9878ee84ced4cb12e560f
marius <mmillea@ucdavis.edu>
2013-09-17 13:35:25 -0700

    camb doc

--- cosmoslik_plugins/models/camb/camb.py
+++ cosmoslik_plugins/models/camb/camb.py
@@ -8,58 +8,6 @@ from cosmoslik import SlikPlugin, SubprocessExtension
 
 class camb(SlikPlugin):
     """
-    ====
-    CAMB
-    ====
-    
-    - Written by Antony Lewis and Anthony Challinor (see `<http://camb.info>`_)
-    - CosmoSlik module by Marius Millea
-    
-    
-    Usage
-    =====
-    
-    To use this module, add ``camb`` to the list of ``models``.
-    
-    Notes
-    =====
-    
-    CosmoSlik uses a modified CAMB which under-the-hood reads the ini file 
-    from `stdin` and writes results to `stdout`. This module also handles some
-    of the logic in chosing settings. In particular,
-    
-    - It automatically choses which of ``get_scalar``, ``get_tensor``, 
-      ``get_vector``, and ``get_transfer`` should be set to true based 
-      on which likelihoods are used. 
-    - It automatically sets ``do_lensing=True`` if ``Alens>0``.
-    - If automatically increases ``lmax`` used in calculations to ensure 
-      full accuracy of outputs up to the specified ``lmax``. 
-    
-    .. note:: 
-    
-        For users wishing to modify or add functionality to CAMB, we recommend starting
-        by making a copy of this module and leaving ``inidriver.F90`` mostly
-        intact (it contains the under-the-hood changes mentioned above.)
-    
-    Parameters
-    ==========
-    
-    [camb].X
-    --------
-        X can be any valid CAMB parameter that you would normally give in the
-        CAMB ini file. These parameters are passed straight to CAMB (and can
-        be sampled over).  
-        
-    [camb].defaults
-    ---------------
-        Path to a CAMB ini file which specifies default values for parameters not set 
-        by the user or by CosmoSlik. (default: defaults.ini in the module folder) 
-        
-    Alens
-    -----
-        cl = Alens * cl_lensed + (1-Alens) * cl_scalar 
-    
-    
     """
     
     name_mapping = {'H0':'hubble',
