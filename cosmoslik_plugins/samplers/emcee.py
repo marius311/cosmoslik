0eaafb98f9bac99bf4f586cfc1d6bd67566ddb5e
marius <mmillea@ucdavis.edu>
2016-08-26 00:29:16 +0200

    emcee sampler can write output_file

--- cosmoslik_plugins/samplers/emcee.py
+++ cosmoslik_plugins/samplers/emcee.py
@@ -3,8 +3,10 @@ from cosmoslik import mpi
 from cosmoslik.chains.chains import combine_covs
 from emcee import EnsembleSampler
 from collections import OrderedDict, namedtuple
-from numpy import ix_, zeros, ceil
+from numpy import ix_, zeros, ceil, hstack, dtype, empty, ones
 from numpy.random import multivariate_normal
+import pickle
+from itertools import chain
 
 mcmcsample = namedtuple("mcmcsample",["weight","lnl","x"])
 
@@ -16,7 +18,9 @@ class emcee(SlikSampler):
         num_samples,
         nwalkers=100,
         proposal_cov=None,
-        output_extra_params=None
+        output_extra_params=None,
+        output_file=None,
+        output_freq=10
     ):
     
         if output_extra_params is None: output_extra_params = []
@@ -26,10 +30,7 @@ class emcee(SlikSampler):
         self.sampled = params.find_sampled()
         self.x0 = [params[k].start for k in self.sampled]
         self.proposal_cov = self.initialize_covariance(self.sampled)
-        if mpi.get_size()>1:
-            self.pool = mpi.get_pool()
-        else:
-            self.pool = None
+        self.pool = mpi.get_pool() if mpi.get_size()>1 else None
     
     def initialize_covariance(self, sampled):
         """
@@ -51,6 +52,18 @@ class emcee(SlikSampler):
                  
     def sample(self, lnl):
         
+        # set up output file
+        if self.output_file and mpi.is_master():
+            _output_file = open(self.output_file,"wb")
+            protocol = pickle.HIGHEST_PROTOCOL
+            pickle.dump(
+                obj=list(chain(['lnl','weight'], self.sampled.keys(), self.output_extra_params.keys())),
+                file=_output_file,
+                protocol=protocol
+            )
+            dtypes = ','.join([dtype(float).name]*(2+len(self.sampled))+list(self.output_extra_params.values()))
+            samples = {i:empty(self.output_freq,dtypes) for i in range(self.nwalkers)}
+        
         # distribute walkers initially according to covariance estimate
         pos = multivariate_normal(
             [v.start for v in self.sampled.values()],
@@ -63,16 +76,30 @@ class emcee(SlikSampler):
             l, p = lnl(*x)
             return -l, p 
                         
-        # step each walker once, yield them one-by-one, repeat
-        weight = zeros(self.nwalkers)
+        # step each walker once, yield them all one-by-one, repeat
+        weight = ones(self.nwalkers)
+        isample = zeros(self.nwalkers,dtype=int)
         sampler = EnsembleSampler(self.nwalkers, len(self.sampled), lnprob, pool=self.pool)
 
-        for i in range(int(ceil(self.num_samples/self.nwalkers))):
+        nsteps = int(ceil(self.num_samples/self.nwalkers))
+        for i in range(nsteps):
             posnext, prob, state, blobs = sampler.run_mcmc(pos,1)
-            for i, (x, xnext, l) in enumerate(zip(pos,posnext,prob)):
-                if (x==xnext).all(): 
-                    weight[i] += 1
+            
+            for iwalker, (x, xnext, l, params) in enumerate(zip(pos,posnext,prob,blobs)):
+                if (x==xnext).all() and i!=nsteps-1: 
+                    weight[iwalker] += 1
                 else:
-                    yield mcmcsample(weight[i]+1,-l,x)
-                    weight[i] = 0
+                    yield mcmcsample(weight[iwalker],-l,x)
+                    
+                    # write to file once every `self.output_freq` accepted steps (per walker)
+                    if self.output_file and mpi.is_master():
+                        row = tuple(chain([-l,weight[iwalker]],x,[params[k] for k in self.output_extra_params]))
+                        samples[iwalker][isample[iwalker]] = row
+                        isample[iwalker] += 1
+                        if isample[iwalker]>=self.output_freq or i==nsteps-1:
+                            pickle.dump((iwalker,samples[iwalker][:isample[iwalker]]),_output_file,protocol)
+                            isample[iwalker] = 0
+                    
+                    weight[iwalker] = 1
+                    
             pos = posnext
