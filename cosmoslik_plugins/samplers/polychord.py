c2ce5d0c23ccd0a5048c776ddaf825bf7d362575
marius <mmillea@ucdavis.edu>
2017-10-18 00:43:50 +0200

    allow derived parameters for polychord sampler

--- cosmoslik_plugins/samplers/polychord.py
+++ cosmoslik_plugins/samplers/polychord.py
@@ -1,4 +1,4 @@
-import os.path as osp
+import os, os.path as osp
 from cosmoslik import *
 from cosmoslik import mpi
 from PyPolyChord import run_polychord
@@ -11,6 +11,7 @@ class polychord(SlikSampler):
         self,
         params,
         output_file,
+        output_extra_params=[],
         nlive=None,
         num_repeats=None,
         do_clustering=None,
@@ -22,9 +23,11 @@ class polychord(SlikSampler):
 
         self.sampled = params.find_sampled()
         
-        settings = self.settings = PolyChordSettings(len(self.sampled), 0)
+        settings = self.settings = PolyChordSettings(len(self.sampled), len(self.output_extra_params))
+        output_file = './'+osp.normpath(output_file)
         settings.base_dir = osp.dirname(output_file)
         settings.file_root = osp.basename(output_file)
+        if mpi.is_master() and not osp.exists(self.settings.base_dir): os.makedirs(self.settings.base_dir)
         for k in ['nlive','num_repeats','do_clustering','read_resume']:
             v = arguments()[k]
             if v is not None:
@@ -33,16 +36,25 @@ class polychord(SlikSampler):
     def sample(self, lnl):
         
         def likelihood(theta):
-            return -lnl(*theta)[0], []
+            l,p = lnl(*theta)
+            return -l, [p[k] for k in self.output_extra_params]
 
         def prior(hypercube):
             theta = [0.0] * len(self.sampled)
             for i,(p,x) in enumerate(zip(self.sampled.values(),hypercube)):
                 if hasattr(p,"min") and hasattr(p,"max"):
                     theta[i] = UniformPrior(p.min,p.max)(x)
+                elif hasattr(p,"range"):
+                    theta[i] = UniformPrior(*p.range)(x)
                 else:
                     raise Exception("To use PolyChord, all parameters must have min/max specified")
             return theta
 
-        output = run_polychord(likelihood, len(self.sampled), 0, self.settings, prior)
-        output.make_paramnames_files([(k,k) for k in self.sampled.keys()])
+        if mpi.is_master():
+            with open(self.output_file+'.paramnames','w') as f:
+                for k in list(self.sampled.keys())+self.output_extra_params:
+                    f.write(k+' '+k+'\n')
+                                       
+        output = run_polychord(likelihood, len(self.sampled), len(self.output_extra_params), self.settings, prior)
+        
+        return []
