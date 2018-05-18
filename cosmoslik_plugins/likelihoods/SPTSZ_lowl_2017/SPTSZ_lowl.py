4913839be0fc442af560321ed0ed5ec2c14198e5
kmaylor <kmaylor@ucdavis.edu>
2018-05-18 12:02:13 -0700

    Added SPTSZ likelihood from Aylor et al

--- cosmoslik_plugins/likelihoods/SPTSZ_lowl_2017/SPTSZ_lowl.py
+++ cosmoslik_plugins/likelihoods/SPTSZ_lowl_2017/SPTSZ_lowl.py
@@ -20,7 +20,7 @@ class SPTSZ_lowl(SlikPlugin):
         self.cal=cal
         self.egfs = SPTSZ_lowl_egfs()
         self.ab_on=ab_on
-        newdat_file = 'data/SPTSZ_bandpowers_and_errors/spt_lps12_20120828/Spectrum_spt2500deg2_lps12_alternativeCalibrationImplementation_no_beam_cov.newdat'   
+        newdat_file = 'data/SPTSZ_bandpowers_and_errors/spt_lps12_20120828/Spectrum_spt2500deg2_lps12_alternativeCalibrationImplementation_no_beam_cov.newdat'
         
         newdat_file = osp.join(osp.dirname(__file__),newdat_file)
             #Load spectrum and covariance
@@ -30,12 +30,14 @@ class SPTSZ_lowl(SlikPlugin):
             self.spec=array([fromstring(f.readline(),sep=' ')[1] for _ in range(47)])
             self.sigma=array([fromstring(f.readline(),sep=' ') for _ in range(47)])
         
+        #Load window functions
         self.windows = array([loadtxt(osp.join(osp.dirname(newdat_file),'windows',window_dir,'window_%i'%i))[:,1] for i in range(1,48)])
-
+        #Get window range to cut theory down to size
         self.windowrange = (lambda x: slice(int(min(x)),int(max(x)+1)))(loadtxt(osp.join(osp.dirname(newdat_file),'windows',window_dir,'window_1'))[:,0])
         self.lmax = self.windowrange.stop
         self.ells = array([dot(arange(10000)[self.windowrange],w) for w in self.windows])
         
+        #Cut out bandpowers not in range of lmin and lmax
         if lmin is not None:
             bmin = sum(1 for _ in takewhile(lambda x: x<lmin, (sum(1 for _ in takewhile(lambda x: abs(x)<.001,w) ) for w in self.windows)))
         else: bmin = 0
@@ -44,7 +46,7 @@ class SPTSZ_lowl(SlikPlugin):
             bmax = sum(1 for _ in takewhile(lambda x: x<lmax, [self.lmax - sum(1 for _ in takewhile(lambda x: abs(x)<.001,reversed(w)) ) for w in self.windows]))
         else: bmax = 48
         
-
+        #Calculate beam correlation matrix
         self.beam_corr = self.spt_beam_cor(freqs=[150,150])[self.windowrange,self.windowrange]
         
         if ((lmin is not None) or (lmax is not None)):
@@ -53,18 +55,18 @@ class SPTSZ_lowl(SlikPlugin):
             self.windows = self.windows[bmin:bmax]
          
     
-
-    def __call__(self, 
+    # returns chi^2 for residual of data and theory
+    def __call__(self,
                  cmb):
             
         self.db,beam_cov = self.get_cl_model_and_beam_cov(cmb,self.egfs)
         #Apply windows and calculate likelihood
         cho_sigma=cho_factor(beam_cov+self.sigma)
         self.deldb = self.spec -self.db*self.cal
-        return dot(self.deldb,cho_solve(cho_sigma, self.deldb))/2. 
+        return dot(self.deldb,cho_solve(cho_sigma, self.deldb))/2.
 
 
-    
+    # Get CMB model add in foregrounds and account for abberation
     def get_cl_model_and_beam_cov(self, cmb, egfs):
         #(Get CMB + foreground model ) * (1+beams_errs)
         self.egfs=egfs(lmax=self.lmax)[:self.lmax]
@@ -73,9 +75,9 @@ class SPTSZ_lowl(SlikPlugin):
         if self.ab_on:
             self.dl*=(1+.26*1.23e-3*self.DlnClDlnl(self.dl))
         #Apply window functions
-        return (dot(self.windows,self.dl), dot(self.windows,dot(self.beam_corr*outer(self.dl,self.dl),self.windows.T )))    
+        return (dot(self.windows,self.dl), dot(self.windows,dot(self.beam_corr*outer(self.dl,self.dl),self.windows.T )))
 
-    
+    # Derivative of ln(CL) wrt ln(l)
     def DlnClDlnl(self,y):
         
         x = arange(self.lmax)[self.windowrange]
@@ -86,14 +88,14 @@ class SPTSZ_lowl(SlikPlugin):
 
 
 
-
-    def spt_beam_cor(self,freqs=[150,150],lmax=4000): 
+    # Function for calculating beam correlation matrix
+    def spt_beam_cor(self,freqs=[150,150],lmax=4000):
 
 
         dir = os.path.dirname(os.path.abspath(__file__)) #abosolute path to current file, keep beam templates in subdirectory
         
         #put beam templates in dictionary labled according to name and year
-        names = ['dc','alpha','wobble','xtalk','outer','inner','venus'] 
+        names = ['dc','alpha','wobble','xtalk','outer','inner','venus']
         freq = '150'
         years = ['2008','2009','2010','2011']
         year_weights = {'2008': 0.065919966,'2009': 0.22589127,'2010': 0.28829848,'2011': 0.41989028}
@@ -101,15 +103,15 @@ class SPTSZ_lowl(SlikPlugin):
         for name in names:
             for year in years:
                     beam_err_file = os.path.join(dir,
-                        'data/SPTSZ_beam_errors/errgrid_'+name+'_'+year+'_'+freq+'.txt') 
+                        'data/SPTSZ_beam_errors/errgrid_'+name+'_'+year+'_'+freq+'.txt')
                     beam_errs[name+year]=loadtxt(beam_err_file)[:,1]
-        mode_names = names[:2]+[names[2:][i]+years[j] for i in range(5) for j in range(4)]      
+        mode_names = names[:2]+[names[2:][i]+years[j] for i in range(5) for j in range(4)]
      
         #we only care about the four year average for dc and alpha so lets do that now
         for year in years:
                 for k in mode_names[:2]:
                     if year == '2008':
-                        beam_errs[k] = beam_errs[k+year]*year_weights[year]                    
+                        beam_errs[k] = beam_errs[k+year]*year_weights[year]
                     else:
                         beam_errs[k] += beam_errs[k+year]*year_weights[year]
                         
@@ -120,12 +122,12 @@ class SPTSZ_lowl(SlikPlugin):
             
         uncorrelated_root_weight = zeros([lmax,lmax])
             
-        uncorrelated = zeros([lmax,lmax])       
+        uncorrelated = zeros([lmax,lmax])
             
             
             #now we convert the beam errors into dCl/Cl and sum them together according to correlation
      
-        for k in mode_names:         
+        for k in mode_names:
             if k in mode_names[:2]:
                 b_err = (1+beam_errs[k][:lmax])**(-2)-1
                 correlated += outer(b_err,b_err)
@@ -138,7 +140,8 @@ class SPTSZ_lowl(SlikPlugin):
                         
         
         return correlated+uncorrelated_root_weight+uncorrelated
-                      
+
+# This class loads foreground templates when inititializeed. The __call__ method returns foreground powerspectrum for given amplitudes
 class SPTSZ_lowl_egfs(SlikPlugin):
     """
     The default foreground model for the SPT low-L likelihood from Story/Keisler
